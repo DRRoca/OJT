b72c7a05860dab53963d61c413ab032a2c9e686e
DRRoca <demsrey.roca@gmail.com>
2018-08-12 20:22:52 +0800

    middleware for notebooks

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,20 @@ Route::group(['middleware'=>'auth'],function() {
     Route::get('/search', 'LiveSearchController@index');
     Route::get('/search/action', 'LiveSearchController@action')->name('LiveSearch.action');
     Route::resource('LiveSearch','LiveSearchController');
+    Route::group(['middleware'=>'notebook.owner'],function() {
+        Route::get('/notebooks/{id}', function() {
+            return view('notes/index');
+        });
+        Route::get('/{notebookId}/createNote','NotesController@createNote')->name('notes.createNote');
+        // Route::get('/notebooks/{id}','NotebooksController@show');
+        // Route::resource('notebooks','NotebooksController');
+    });
+    // Route::get('/notebooks/{id}','NotebooksController@show');
     Route::resource('notebooks','NotebooksController');
     Route::resource('notes','NotesController');
-    Route::get('/{notebookId}/createNote','NotesController@createNote')->name('notes.createNote');
+    // Route::get('/{notebookId}/createNote','NotesController@createNote')->name('notes.createNote');
 });
+
 
 Auth::routes();
 
